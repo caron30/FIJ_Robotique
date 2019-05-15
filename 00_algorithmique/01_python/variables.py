09d3e6f66a8deafcc553281e3d0032451eee2166
caron <caronmulongo30@gmail.com>
2019-05-15 16:20:08 +0200

    ajout de variable.py

---
+++ 00_algorithmique/01_python/variables.py
@@ -0,0 +1,5 @@
+# il existe 3 types de vaiable sur python 
+
+variable1= 5 # integer >int qui symbolise un nombre entièr 
+variable2 = 3.2 # float > float qui symbolise chiffre a virgule 
+variable3 = "coucou" # strig > str  qui symbolise du texte 
